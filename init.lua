69ff69998d4afd87f5d329568e96b08721cf4751
pbaekgaard <pbakga21@student.aau.dk>
2025-06-06 09:38:46 +0200

    added cpp to treesitter ensure installed

--- init.lua
+++ init.lua
@@ -942,7 +942,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'cpp' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
