d68d5518b4e36c16fe1f4483d033995b28eadc3d
pbaekgaard <pbakga21@student.aau.dk>
2025-08-04 20:38:58 +0200

    start

--- init.lua
+++ init.lua
@@ -56,5 +56,50 @@ styles = {
 		transparency = true,
 	}
 })
+
+    require('oil').setup {
+      default_file_explorer = true,
+      delete_to_trash = true,
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        show_hidden = true,
+        natural_order = true,
+        is_always_hidden = function(name, _)
+          return name == '..' or name == '.git'
+        end,
+      },
+      float = {
+        padding = 2,
+        max_width = 160,
+        min_width = 50,
+        width = nil,
+        -- max_height = 0,
+        -- min_width = 50,
+      },
+      preview = {
+        max_width = 0.90,
+        min_width = 0.7,
+        width = nil,
+        border = 'rounded',
+        update_on_cursor_moved = true,
+      },
+      win_options = {
+        wrap = true,
+        winblend = 0,
+        winhl = 'Normal:Normal,Normal:Normal',
+      },
+      keymaps = {
+        ['<C-c>'] = false,
+        ['q'] = 'actions.close',
+      },
+    }
+    vim.keymap.set('n', '<leader>e', function()
+      oil = require 'oil'
+      oil.open_float()
+    end, { desc = '[E]xplore Files' })
+
+
+
+
 vim.cmd("colorscheme rose-pine")
 vim.cmd(":hi statusline guibg=NONE")
