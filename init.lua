4e24e245273eec5e0f4fbc6cdfece23fbcec9931
pbd <pbd@deif.com>
2025-11-05 15:08:57 +0100

    removed neo-tree

--- init.lua
+++ init.lua
@@ -1,7 +1,3 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 vim.o.winborder = "rounded"
 require("config.lazy")
-
--- Enable spellchecking and set language to Danish (and optionally English)
-vim.opt_local.spell = false
-vim.opt_local.spelllang = { "da", "en" }
