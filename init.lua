61e33811c2687fbefa25fce82a36003d5a2feb57
pbd <pbd@deif.com>
2025-11-05 14:28:39 +0100

    removed spellcheck

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 vim.o.winborder = "rounded"
 require("config.lazy")
+
+-- Enable spellchecking and set language to Danish (and optionally English)
+vim.opt_local.spell = false
+vim.opt_local.spelllang = { "da", "en" }
---
+++ plugin/after/markdown.lua
@@ -0,0 +1,3 @@
+vim.opt_local.spell = false
+vim.opt_local_spelllang = { "da", "en" }
+vim.o.spl = ""
